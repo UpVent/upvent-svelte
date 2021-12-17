1676af8bec95d1e7e2a87a344149a67f64805868
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-17 01:00:19 -0600

    Add experimental JSON request + Response

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,27 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 use rocket::fs::NamedFile;
+use rocket::http::Status;
+use rocket::response::{content, status};
+
+use rocket::serde::{json::Json, Serialize};
+
+#[serde(crate = "rocket::serde")]
+#[derive(Serialize)]
+struct Testimonial {
+    name: String,
+    title: String,
+    website: String,
+}
+
+#[get("/testimonials")]
+fn testimonials() -> Json<Testimonial> {
+    Json(Testimonial {
+        name: "Juan Camaney López Rodríguez".to_string(),
+        title: "CEO".to_string(),
+        website: "https://github.com".to_string(),
+    })
+}
 
 #[get("/")]
 async fn index() -> io::Result<NamedFile> {
@@ -18,5 +39,5 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, files])
+    rocket::build().mount("/", routes![index, files, testimonials])
 }
