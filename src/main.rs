7ad258c78833120f3c123c10cb62f37048c9ce08
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-16 22:49:52 -0600

    Serve a static website successfully

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 #[macro_use]
 extern crate rocket;
 
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::fs::NamedFile;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html").await
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, files])
 }
