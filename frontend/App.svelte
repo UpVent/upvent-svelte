27628afa0d893e54bb76f0a4b10409fc4b3dd670
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-18 00:38:54 -0600

    Import navbar component

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,8 +1,11 @@
 <script>
+    import Navbar from './Navbar.svelte';
+
 	export let name;
 </script>
 
 <main>
+    <Navbar/>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
