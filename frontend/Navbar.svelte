8e2301f9866b9e653838194b7b9903bd466f57ee
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-18 00:39:16 -0600

    Added basic Navbar without much customization

---
+++ frontend/Navbar.svelte
@@ -0,0 +1,49 @@
+<script>
+    import {
+        Collapse,
+        Dropdown,
+        DropdownItem,
+        DropdownMenu,
+        DropdownToggle,
+        Nav,
+        NavItem,
+        NavLink,
+        Navbar,
+        NavbarBrand,
+        NavbarToggler,
+    } from 'sveltestrap';
+
+    let isOpen = false;
+
+    let logo_white = 'images/logo-white.png'
+
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
+</script>
+
+<Navbar color="primary" dark expand="md">
+    <NavbarBrand href="/">
+        <img width="148" height="48" src="{logo_white}" alt="UpVent Logo" />
+    </NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink href="#components/">Components</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/bestguy/sveltestrap">GitHub</NavLink>
+      </NavItem>
+      <Dropdown nav inNavbar>
+        <DropdownToggle nav caret>Options</DropdownToggle>
+        <DropdownMenu end>
+          <DropdownItem>Option 1</DropdownItem>
+          <DropdownItem>Option 2</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
