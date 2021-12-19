4602260b93cf277b5745c6c14b08bcb4451c394a
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-19 00:23:40 -0600

    Remove unused structs

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,8 @@ use std::io;
 use std::path::{Path, PathBuf};
 
 use rocket::fs::NamedFile;
-use rocket::serde::{json::Json, Serialize};
 
 mod models;
-
-#[serde(crate = "rocket::serde")]
-#[derive(Serialize)]
-struct Testimonial {
-    name: String,
-    testimonial: String,
-    workplace: String,
-    website: String,
-}
-
-#[get("/testimonials")]
-fn testimonials() -> Json<Testimonial> {
-    Json(Testimonial {
-        name: "Juan Camaney López Rodríguez".to_string(),
-        testimonial: "Todo 10/10".to_string(),
-        workplace: "CEO".to_string(),
-        website: "https://github.com".to_string(),
-    })
-}
 
 #[get("/")]
 async fn index() -> io::Result<NamedFile> {
@@ -40,5 +20,5 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, files, testimonials])
+    rocket::build().mount("/", routes![index, files])
 }
