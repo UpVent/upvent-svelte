90efa63233005b104f3f25b7bf1732d297f2a353
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-19 01:07:13 -0600

    Add basic metatags to the main page

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,8 +1,41 @@
 <script>
+    import { MetaTags } from 'svelte-meta-tags';
+
     import Navbar from './Navbar.svelte';
 
-	export let name;
+    export let name;
+
 </script>
+
+<MetaTags
+    title = "UpVent - Soluciones en la nube para tu negocio. Low Cost, siempre listas."
+    description = "UpVent es un proveedor experto de soluciones de código libre y empresarial para los pequeños y medianos negocios Mexicanos."
+    canonical = "https://upvent.codes/"
+
+    openGraph = {{
+        url: 'https://upvent.codes/',
+        title: "UpVent - Soluciones en la nube para tu negocio. Low Cost, siempre listas.",
+        description: "UpVent es un proveedor experto de soluciones de código libre y empresarial para los pequeños y medianos negocios Mexicanos."
+         images: [
+            {
+                url: 'https://www.example.ie/og-image-01.jpg',
+                width: 800,
+                height: 600,
+                alt: 'Og Image Alt'
+            },
+            {
+                url: 'https://www.example.ie/og-image-02.jpg',
+                width: 900,
+                height: 800,
+                alt: 'Og Image Alt Second'
+            },
+            { url: 'https://www.example.ie/og-image-03.jpg' },
+            { url: 'https://www.example.ie/og-image-04.jpg' }
+        ],
+        site_name: 'SiteName'
+    }}
+
+/>
 
 <Navbar/>
 
