94da1118a644abc781c5a759346e0797f8998197
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-19 02:54:31 -0600

    Improve navbar syntax

--- frontend/Navbar.svelte
+++ frontend/Navbar.svelte
@@ -9,8 +9,9 @@
         faUsers,
     } from '@fortawesome/free-solid-svg-icons';
 
+    import { Button } from 'sveltestrap';
 
-    let logo = 'images/logo-grey.png';
+    const logo = 'images/logo-grey.png';
 </script>
 
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-0">
@@ -19,14 +20,13 @@
     </a>
 
     <ul class="nav nav-pills col-12 mx-auto col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a class="nav-link px-2 link-dark" href="/">Inicio <Fa icon={faHome} /></a></li>
-        <li><a class="nav-link px-2 link-dark" href="/">Blog <Fa icon={faPen} /></a></li>
-        <li><a class="nav-link px-2 link-dark" href="/">Nosotros <Fa icon={faUsers} /></a></li>
-        <li><a class="nav-link px-2 link-dark" href="/">Servicios <Fa icon={faLaptop} /></a></li>
-        <li><a class="nav-link px-2 link-dark" href="/">Contacto <Fa icon={faPhone} /></a></li>
-        <li><a class="nav-link px-2 link-dark" href="/">MarketCloud <Fa icon={faShoppingBag} /></a></li>
+        <li><a class="nav-link px-2 text-muted" href="/">Inicio</a></li>
+        <li><a class="nav-link px-2 text-muted" href="/">Blog</a></li>
+        <li><a class="nav-link px-2 text-muted" href="/">Nosotros</a></li>
+        <li><a class="nav-link px-2 text-muted" href="/">Servicios</a></li>
+        <li><a class="nav-link px-2 text-muted" href="/">MarketCloud</a></li>
     </ul>
     <div class="text-end me-auto ms-auto mx-auto">
-        <button type="button" class="btn btn-light border-0" title="Modo daltonismo">👁</button>
+        <Button size="lg" color="primary">Contacto <Fa icon={faPhone} /></Button>
     </div>
 </header>
