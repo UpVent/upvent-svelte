b056bfdfa35029f1b889410994b853c17e2dac47
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-28 20:58:18 -0600

    Restore main rocket rs file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate diesel;
+
+use rocket::fs::NamedFile;
+use std::io;
+
+#[get("/")]
+async fn index() -> io::Result<NamedFile> {
+    NamedFile::open("public/index.html").await
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
