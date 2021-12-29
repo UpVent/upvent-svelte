185bf3f9fc7c158c95f1ed73e0afc17ad9a19632
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-28 22:04:04 -0600

    Create initial migration for models

---
+++ migrations/2021-12-29-030413_create_models/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+CREATE TABLE testimonials (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    testimonial TEXT NOT NULL,
+    workplace TEXT NOT NULL,
+    website TEXT NOT NULL
+);
