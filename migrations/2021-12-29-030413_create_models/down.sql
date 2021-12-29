fbce3644fd73c95d046076fd1cbd167fc3bce5e0
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-29 01:42:55 -0600

    Safer drop table SQL queries

--- migrations/2021-12-29-030413_create_models/down.sql
+++ migrations/2021-12-29-030413_create_models/down.sql
@@ -1,11 +1,11 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE testimonials;
-DROP TABLE projects;
-DROP TABLE fsprojects;
-DROP TABLE licenses;
-DROP TABLE hofs;
-DROP TABLE teammembers;
-DROP TABLE privacypolicies;
-DROP TABLE termsofservices;
-DROP TABLE posts;
-DROP TABLE products;
+DROP TABLE IF EXISTS testimonials;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS fsprojects;
+DROP TABLE IF EXISTS licenses;
+DROP TABLE IF EXISTS hofs;
+DROP TABLE IF EXISTS teammembers;
+DROP TABLE IF EXISTS privacypolicies;
+DROP TABLE IF EXISTS termsofservices;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS products;
