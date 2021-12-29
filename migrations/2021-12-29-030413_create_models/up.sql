64febb9dd7d91906ced48ef8c592455896c45299
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-28 22:20:06 -0600

    Update fields (1/2)

--- migrations/2021-12-29-030413_create_models/up.sql
+++ migrations/2021-12-29-030413_create_models/up.sql
@@ -1,8 +1,43 @@
 -- Your SQL goes here
+
+/* HOMEPAGE testimonials  */
 CREATE TABLE testimonials (
     id TEXT PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     testimonial TEXT NOT NULL,
     workplace TEXT NOT NULL,
     website TEXT NOT NULL
+);
+
+/* Project used in the portfolio section in the ABOUT US page */
+CREATE TABLE projects (
+    id TEXT PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    site TEXT NOT NULL,
+    description TEXT NOT NULL
+);
+
+/* Free Software project shown in the Services page of the site */
+CREATE TABLE fsprojects (
+    id TEXT PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    github_addr TEXT NOT NULL,
+    support_addr TEXT NOT NULL,
+    proj_license TEXT NOT NULL,
+    license_link TEXT NOT NULL
+);
+
+/* License text shown in the LICENSES page */
+CREATE TABLE licenses (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    verbatim TEXT NOT NULL,
+    license_link TEXT NOT NULL
+);
+
+/* Hall Of Fame project */
+CREATE TABLE hofs (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL
 );
--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,41 @@
 table! {
+    fsprojects (id) {
+        id -> Text,
+        title -> Text,
+        description -> Text,
+        github_addr -> Text,
+        support_addr -> Text,
+        proj_license -> Text,
+        license_link -> Text,
+    }
+}
+
+table! {
+    hofs (id) {
+        id -> Text,
+        name -> Text,
+    }
+}
+
+table! {
+    licenses (id) {
+        id -> Text,
+        name -> Text,
+        verbatim -> Text,
+        license_link -> Text,
+    }
+}
+
+table! {
+    projects (id) {
+        id -> Text,
+        title -> Text,
+        site -> Text,
+        description -> Text,
+    }
+}
+
+table! {
     testimonials (id) {
         id -> Text,
         name -> Text,
@@ -7,3 +44,11 @@ table! {
         website -> Text,
     }
 }
+
+allow_tables_to_appear_in_same_query!(
+    fsprojects,
+    hofs,
+    licenses,
+    projects,
+    testimonials,
+);
