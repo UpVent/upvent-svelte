a45a2bb9625ecd4cb9c2346e892d812eb3609d93
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2021-12-31 21:27:14 -0600

    Add insert test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ extern crate rocket;
 #[macro_use]
 extern crate diesel;
 
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
 use rocket::fs::NamedFile;
+use std::env;
 use std::io;
 
 /* Crate modules */
@@ -18,5 +22,23 @@ async fn index() -> io::Result<NamedFile> {
 
 #[launch]
 fn rocket() -> _ {
+    dotenv().ok();
+
+    let db_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    let conn = SqliteConnection::establish(&db_url).unwrap();
+
+    let testimonial = models::NewTestimonial {
+        name: String::from("VentGrey"),
+        testimonial: String::from("This testimonial text is a test. The cake is a lie!"),
+        workplace: String::from("UpVent Technologies"),
+        website: String::from("https://upvent.codes/"),
+    };
+
+    if models::Testimonial::insert(testimonial, &conn) {
+        println!("Testimonial inserted correctly!");
+    } else {
+        println!("Something failed while inserting the testimonial!");
+    }
+
     rocket::build().mount("/", routes![index])
 }
