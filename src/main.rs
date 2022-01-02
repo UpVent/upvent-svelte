974c4d00e538aa206efa2f804b53c2394dfd1f3d
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-02 02:01:53 -0600

    Comment out insert. Move this to "tests" when testing suites are implemented

--- src/main.rs
+++ src/main.rs
@@ -31,21 +31,21 @@ async fn all(file: PathBuf) -> Option<NamedFile> {
 fn rocket() -> _ {
     dotenv().ok();
 
-    let db_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
-    let conn = SqliteConnection::establish(&db_url).unwrap();
+    //    let db_url = env::var("DATABASE_URL").expect("set DATABASE_URL");
+    //    let conn = SqliteConnection::establish(&db_url).unwrap();
+    //
+    //    let testimonial = models::NewTestimonial {
+    //        name: String::from("VentGrey"),
+    //        testimonial: String::from("This testimonial text is a test. The cake is a lie!"),
+    //        workplace: String::from("UpVent Technologies"),
+    //        website: String::from("https://upvent.codes/"),
+    //    };
+    //
+    //    if models::Testimonial::insert(testimonial, &conn) {
+    //        println!("Testimonial inserted correctly!");
+    //    } else {
+    //        println!("Something failed while inserting the testimonial!");
+    //    }
 
-    let testimonial = models::NewTestimonial {
-        name: String::from("VentGrey"),
-        testimonial: String::from("This testimonial text is a test. The cake is a lie!"),
-        workplace: String::from("UpVent Technologies"),
-        website: String::from("https://upvent.codes/"),
-    };
-
-    if models::Testimonial::insert(testimonial, &conn) {
-        println!("Testimonial inserted correctly!");
-    } else {
-        println!("Something failed while inserting the testimonial!");
-    }
-
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![all, index])
 }
