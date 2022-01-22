1b187255de0fa6777b04be3ba333e29d11f9f49d
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-21 18:27:35 -0600

    Order app.svelte

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,13 +1,14 @@
 <script>
-    import { MetaTags } from 'svelte-meta-tags';
-
     import Navbar from './Navbar.svelte';
     import IndexJumbotron from './IndexJumbotron.svelte';
     import Footer from './Footer.svelte';
-
-    export let name;
 </script>
 
+<!-- Navbar Component -->
 <Navbar/>
+
+<!-- Index jumbotron component -->
 <IndexJumbotron/>
+
+<!-- Footer component -->
 <Footer/>
