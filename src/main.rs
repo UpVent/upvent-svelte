72cf5ebd3ce6866f797bfefd9633b55956d3f8f4
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-22 00:15:02 -0600

    Implement CORS in main server

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ extern crate rocket;
 extern crate diesel;
 
 use dotenv::dotenv;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
 use rocket::serde::json::{json, Json, Value};
+use rocket::{Request, Response};
 use std::env;
 
 /* Crate modules */
@@ -61,6 +64,28 @@ pub fn not_found() -> Json<Value> {
     }))
 }
 
+struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
@@ -69,6 +94,7 @@ fn rocket() -> _ {
 
     rocket::build()
         .manage(pool)
+        .attach(CORS)
         .mount(
             "/api/",
             routes![
