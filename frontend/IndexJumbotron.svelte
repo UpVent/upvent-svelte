ff684c2ff68466cca449471432a484885cb0d36f
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-21 18:40:15 -0600

    Reset content

--- frontend/IndexJumbotron.svelte
+++ frontend/IndexJumbotron.svelte
@@ -9,25 +9,6 @@
 
 </script>
 
-<Container>
-    <Row>
-        <Col xs="auto">
-            <h1 class="display-4 fw-bold">Nube inteligente para negocios inteligentes.</h1>
-            <p class="small text-muted fs-5">Somos una compañía de software empresarial, basados en un modelo
-                de desarrollo de software libre. En UpVent construimos el futuro
-                del software libre, resolviendo problemas y otorgando un sentido
-                de libertad a nuestros clientes.
-            </p>
-            <Row>
-                <Col xs="auto">
-                    <ButtonGroup class="shadow-sm">
-                        <Button color="primary" >Comenzar</Button>
-                        <Button active color="dark">Explorar Software</Button>
-                    </ButtonGroup>
-                </Col>
-            </Row>
-        </Col>
-        <Col xs="auto">.col-auto - variable width content</Col>
-    </Row>
-
-</Container>
+<section>
+    
+</section>
