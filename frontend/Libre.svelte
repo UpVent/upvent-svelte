afdb1cf590564a4c049e90fa5c1555e92cc4a71a
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-04 21:49:06 -0600

    Beautify text

--- frontend/Libre.svelte
+++ frontend/Libre.svelte
@@ -4,10 +4,16 @@
     let gnu = 'images/gnu.png';
 </script>
 
+<style>
+    .text-glow {
+        text-shadow: 1px 1px 1px #007BFC;
+    }
+</style>
+
 <section class="container mt-5 mb-5">
     <div class="container text-center">
         <img class="d-block mx-auto mb-4" src={gnu} alt="Logo de UpVent circular" width="150" height="150" loading="lazy"/>
-        <p class="h2 fw-bold">Todo lo que hacemos es de código libre</p>
+        <p class="h2 fw-bold">Todo lo que hacemos es de</p> <span class="h2 fw-bold text-glow text-primary">código libre</span>
         <p class="text-center text-wrap lead">
                 Nuestro modelo de desarrollo consiste totalmente de componentes de código
                 libre y se basa en múltiples principios éticos y prácticos para su conveniencia como empresa.
