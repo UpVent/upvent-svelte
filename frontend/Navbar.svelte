f3f7ba43d40fa6affbe0835c27cfb2941c60343a
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-05 00:52:01 -0600

    Add contact form to navbar as part of it

--- frontend/Navbar.svelte
+++ frontend/Navbar.svelte
@@ -5,10 +5,17 @@
     import Laptop from 'svelte-bootstrap-icons/lib/Laptop';
     import Bag from 'svelte-bootstrap-icons/lib/Bag';
     import Phone from 'svelte-bootstrap-icons/lib/Phone';
+    import Envelope from 'svelte-bootstrap-icons/lib/Envelope';
 
-    import { Button } from 'sveltestrap';
+
+    import { Button, Modal } from 'sveltestrap';
 
     const logo = 'images/logo-grey.png';
+    const form_id = 'moqrekzb';
+
+    // Modal variable
+    let open = false;
+    const toggle = () => (open = !open);
 </script>
 
 <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-0">
@@ -25,6 +32,22 @@
     </ul>
 
     <div class="text-end me-auto ms-auto mx-auto">
-        <Button size="lg" color="primary">Contacto <Phone/></Button>
+        <Button size="lg" color="primary" on:click={toggle} >Contacto <Phone/></Button>
     </div>
 </header>
+
+<!-- Modal de contacto -->
+<Modal body header="¡Gracias por contactarnos!" isOpen={open} {toggle}>
+    <form class="form" id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/{form_id}" method="post">
+    <fieldset id="fs-frm-inputs">
+    <label class="form-label" for="full-name">Nombre Completo</label>
+    <input class="form-control" type="text" name="name" id="full-name" placeholder="Su nombre aquí..." required="">
+    <label class="form-label" for="email-address">Correo Electronico</label>
+    <input class="form-control" type="email" name="_replyto" id="email-address" placeholder="email@dominio.com" required="">
+    <label class="form-label" for="message">Mensaje</label>
+    <textarea class="form-control" rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
+    <input class="form-control" type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
+  </fieldset>
+  <input class="btn btn-primary mt-3 mb-3" type="submit" value="Enviar ✉️">
+</form>
+</Modal>
