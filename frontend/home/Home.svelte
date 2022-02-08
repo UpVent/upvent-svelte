d4fdf830a4bd8f6bd09ed4f97febd7e22cae1f17
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-07 18:50:10 -0600

    Move components

---
+++ frontend/home/Home.svelte
@@ -0,0 +1,27 @@
+<script>
+    import IndexJumbotron from './IndexJumbotron.svelte';
+    import Features from './Features.svelte';
+    import Testimonials from './Testimonials.svelte';
+    import Libre from './Libre.svelte';
+    import Benefits from './Benefits.svelte';
+    import StartNow from './StartNow.svelte';
+</script>
+
+<!-- Index jumbotron component -->
+<IndexJumbotron/>
+
+<!-- Features component -->
+<Features/>
+
+<!-- Testimonials component -->
+<Testimonials/>
+
+<!-- Libre Software component -->
+<Libre/>
+
+<!-- Benefits component -->
+<Benefits/>
+
+<!-- StartNow component -->
+<StartNow/>
+
