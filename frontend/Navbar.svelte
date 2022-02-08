b923d577af0b689d4fe143d07726944e7955dc18
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-07 18:13:56 -0600

    Fix indentation

--- frontend/Navbar.svelte
+++ frontend/Navbar.svelte
@@ -7,7 +7,6 @@
     import People from 'svelte-bootstrap-icons/lib/People';
     import Phone from 'svelte-bootstrap-icons/lib/Phone';
 
-
     import { Button, Modal } from 'sveltestrap';
 
     const logo = 'images/logo-grey.png';
@@ -25,11 +24,14 @@
 
     <ul class="nav nav-pills col-12 mx-auto col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a class="nav-link px-2 text-muted" href="/">Inicio <House/></a></li>
-        <li><a class="nav-link px-2 text-muted" href="/">Blog <Pen/></a></li>
+        <li><a class="nav-link px-2 text-muted" href="/blog">Blog <Pen/></a></li>
         <li><a class="nav-link px-2 text-muted" href="/">Nosotros <People/></a></li>
         <li><a class="nav-link px-2 text-muted" href="/">Servicios <Laptop/></a></li>
         <li><a class="nav-link px-2 text-muted" href="/">MarketCloud <Bag/></a></li>
     </ul>
+
+
+    <Route path="/blog"><Blog /></Route>
 
     <div class="text-end me-auto ms-auto mx-auto">
         <Button size="lg" color="primary" on:click={toggle} >Contacto <Phone/></Button>
@@ -39,15 +41,15 @@
 <!-- Modal de contacto -->
 <Modal body header="¡Gracias por contactarnos!" isOpen={open} {toggle}>
     <form class="form" id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/{form_id}" method="post">
-    <fieldset id="fs-frm-inputs">
-    <label class="form-label" for="full-name">Nombre Completo</label>
-    <input class="form-control" type="text" name="name" id="full-name" placeholder="Su nombre aquí..." required="">
-    <label class="form-label" for="email-address">Correo Electronico</label>
-    <input class="form-control" type="email" name="_replyto" id="email-address" placeholder="email@dominio.com" required="">
-    <label class="form-label" for="message">Mensaje</label>
-    <textarea class="form-control" rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
-    <input class="form-control" type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
-  </fieldset>
-  <input class="btn btn-primary mt-3 mb-3" type="submit" value="Enviar ✉️">
-</form>
+        <fieldset id="fs-frm-inputs">
+        <label class="form-label" for="full-name">Nombre Completo</label>
+        <input class="form-control" type="text" name="name" id="full-name" placeholder="Su nombre aquí..." required="">
+        <label class="form-label" for="email-address">Correo Electronico</label>
+        <input class="form-control" type="email" name="_replyto" id="email-address" placeholder="email@dominio.com" required="">
+        <label class="form-label" for="message">Mensaje</label>
+        <textarea class="form-control" rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
+        <input class="form-control" type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
+    </fieldset>
+    <input class="btn btn-primary mt-3 mb-3" type="submit" value="Enviar ✉️">
+    </form>
 </Modal>
