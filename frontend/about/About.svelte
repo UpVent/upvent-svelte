182d25ffb395d115cdaf068197c168247d5fc9d1
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-10 22:13:28 -0600

    Add Reasons component to about page

--- frontend/about/About.svelte
+++ frontend/about/About.svelte
@@ -1,8 +1,10 @@
 <script>
     import Aboutjumbotron from './AboutJumbotron.svelte';
     import Tools from './Tools.svelte';
+    import Reasons from './Reasons.svelte';
 </script>
 
 <!-- Jumbotron About -->
 <Aboutjumbotron/>
 <Tools/>
+<Reasons/>
