e9345ac40afb19aa999f054be4be8bd8bd8641dd
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-12 22:51:31 -0600

    Add a new webpages section

---
+++ frontend/services/Webpages.svelte
@@ -0,0 +1,46 @@
+<script charset="utf-8">
+    
+</script>
+
+<style>
+    
+</style>
+
+<section class="container mt-5 mb-5">
+        <div class="row text-right align-items-center">
+            <div class="col-12 col-lg-6 col-xl-5 m-lg-auto text-left">
+                <h1 class="text-center fw-bold display-3">Páginas Web</h1>
+                <div class="row pt-5">
+                    <div class="col-3">
+                        <img alt="Ícono de un pincel" class="img-fluid" src="{% static 'core/img/brush.svg' %}" loading="lazy">
+                    </div>
+                    <div class="col-9">
+                        <h2>Desarrollo Personalizado</h2>
+                        <p class="text-muted text-wrap">
+                            Desarrollamos su sitio web de acuerdo a sus necesidades, sea una pequeña empresa, freelancer o una corporación.
+                            <br/>
+                            UpVent estará ahí para ofrecerle una solución única.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row pt-5">
+                    <div class="col-9 text-right">
+                        <h2>Velocidad y Seguridad</h2>
+                        <p class="text-muted text-wrap">
+                            Creamos páginas web veloces utilizando una mezcla de tecnologías propias y tecnologías libres.
+                            <br />
+                            Gracias a la tecnología de LiteSpeed podemos ofrecerle sitios seguros y en producción casi de inmediato.
+                        </p>
+                    </div>
+                    <div class="col-3">
+                        <img alt="Ícono de un rayo" class="img-fluid" src="{% static 'core/img/bolt.svg' %}" loading="lazy">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-7 col-sm-4 m-auto pt-5 pt-md-0">
+                <img alt="Imágen mostrando una computadora" class="img-fluid" src="{% static 'core/img/responsive.svg' %}" loading="lazy">
+            </div>
+        </div>
+    </section>
