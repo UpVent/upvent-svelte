c0d6273fb7b2b420dc738e181c901655425c9613
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-02-12 23:06:19 -0600

    Add webpages section

--- frontend/services/Services.svelte
+++ frontend/services/Services.svelte
@@ -2,9 +2,11 @@
     import ServiceJumbotron from './ServiceJumbotron.svelte';
     import Cloud from './Cloud.svelte';
     import Plans from './Plans.svelte';
+    import Webpages from './Webpages.svelte';
 </script>
 
 <ServiceJumbotron/>
 <Cloud/>
 <Plans/>
+<Webpages/>
 
--- frontend/services/Webpages.svelte
+++ frontend/services/Webpages.svelte
@@ -1,47 +1,44 @@
 <script>
     const bolt = '/images/bolt.svg';
+    const brush = '/images/brush.svg';
     const responsive = '/images/responsive.svg';
 </script>
 
-<style>
-    
-</style>
-
 <section class="container mt-5 mb-5">
-        <div class="row text-right align-items-center">
-            <div class="col-12 col-lg-6 col-xl-5 m-lg-auto text-left">
-                <h1 class="text-center fw-bold display-3">Páginas Web</h1>
-                <div class="row pt-5">
-                    <div class="col-3">
-                        <img alt="Ícono de un pincel" class="img-fluid" src="{% static 'core/img/brush.svg' %}" loading="lazy">
-                    </div>
-                    <div class="col-9">
-                        <h2>Desarrollo Personalizado</h2>
-                        <p class="text-muted text-wrap">
-                            Desarrollamos su sitio web de acuerdo a sus necesidades, sea una pequeña empresa, freelancer o una corporación.
-                            <br/>
-                            UpVent estará ahí para ofrecerle una solución única.
-                        </p>
-                    </div>
+    <div class="row text-right align-items-center">
+        <div class="col-12 col-lg-6 col-xl-5 m-lg-auto text-left">
+            <h1 class="text-center fw-bold display-3">Páginas Web</h1>
+            <div class="row pt-5">
+                <div class="col-3">
+                    <img alt="Ícono de un pincel" class="img-fluid" src={brush}>
                 </div>
-
-                <div class="row pt-5">
-                    <div class="col-9 text-right">
-                        <h2>Velocidad y Seguridad</h2>
-                        <p class="text-muted text-wrap">
-                            Creamos páginas web veloces utilizando una mezcla de tecnologías propias y tecnologías libres.
-                            <br />
-                            Gracias a la tecnología de LiteSpeed podemos ofrecerle sitios seguros y en producción casi de inmediato.
-                        </p>
-                    </div>
-                    <div class="col-3">
-                        <img alt="Ícono de un rayo" class="img-fluid" src={bolt}>
-                    </div>
+                <div class="col-9">
+                    <h2>Desarrollo Personalizado</h2>
+                    <p class="text-muted text-wrap">
+                        Desarrollamos su sitio web de acuerdo a sus necesidades, sea una pequeña empresa, freelancer o una corporación.
+                        <br/>
+                        UpVent estará ahí para ofrecerle una solución única.
+                    </p>
                 </div>
             </div>
 
-            <div class="col-7 col-sm-4 m-auto pt-5 pt-md-0">
-                <img alt="Imágen mostrando una computadora" class="img-fluid" src={responsive}>
+            <div class="row pt-5">
+                <div class="col-9 text-right">
+                    <h2>Velocidad y Seguridad</h2>
+                    <p class="text-muted text-wrap">
+                        Creamos páginas web veloces utilizando una mezcla de tecnologías propias y tecnologías libres.
+                        <br />
+                        Gracias a la tecnología de LiteSpeed podemos ofrecerle sitios seguros y en producción casi de inmediato.
+                    </p>
+                </div>
+                <div class="col-3">
+                    <img alt="Ícono de un rayo" class="img-fluid" src={bolt}>
+                </div>
             </div>
         </div>
+
+        <div class="col-7 col-sm-4 m-auto pt-5 pt-md-0">
+            <img alt="Imágen mostrando una computadora" class="img-fluid" src={responsive}>
+        </div>
+    </div>
 </section>
