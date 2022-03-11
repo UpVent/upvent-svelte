9f815f246c0fef9bacca84817b6d7aaf5a18f645
VentGrey <omarpurataf@gmail.com>
2022-03-11 16:56:47 -0600

    Remove unsupported lazy loading

--- frontend/Footer.svelte
+++ frontend/Footer.svelte
@@ -14,7 +14,7 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                     <div class="container">
-                        <img class="img-fluid mb-1" width="116" height="47" src="images/logo-grey.png" alt="UpVent Logo" loading="lazy"/>
+                        <img class="img-fluid mb-1" width="116" height="47" src="images/logo-grey.png" alt="UpVent Logo"/>
                     </div>
                     <p>Todos los derechos reservados © - UpVent {year} . Todos los logos son marcas registradas de sus respectivos dueños.</p>
                 </div>
