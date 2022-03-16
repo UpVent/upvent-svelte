8775d01f78a7684ab0da89d628b81e441346d6ac
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-03-15 23:50:31 -0600

    Optimized png images

--- frontend/home/Stats.svelte
+++ frontend/home/Stats.svelte
@@ -1,7 +1,18 @@
 <script>
-    
+ 
 </script>
 
 <style>
     
 </style>
+
+<section class="container">
+    <p class="h1">El secreto para hacer crecer su negocio en <span class="text-primary text-glow">días.</span></p>
+    <p class="text-muted">¿Sabía que 5 de cada 10 empresas Mexicanas suplican su crecimiento y 2 de cada 10 registran un crecimiento del 300% aproximadamente en su volumen de ventas?</p>
+
+    <!-- Figura -->
+    <figure>
+        <figcaption class="figure caption">Fuente: Reporte de impacto sobre el COVID-19 en Venta Online, elaborado por la Asociación Mexicana de Venta Online (AMVO)</figcaption>
+    </figure>
+
+</section>
