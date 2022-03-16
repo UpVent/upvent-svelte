6409f1b3f36ca521bdadd5862c30bf089c0544fb
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-03-15 23:57:55 -0600

    Remove unused module

--- frontend/home/Home.svelte
+++ frontend/home/Home.svelte
@@ -1,7 +1,6 @@
 <script>
     import IndexJumbotron from './IndexJumbotron.svelte';
     import Features from './Features.svelte';
-    import Stats from './Stats.svelte';
     import Libre from './Libre.svelte';
     import Benefits from './Benefits.svelte';
     import StartNow from './StartNow.svelte';
@@ -12,9 +11,6 @@
 
 <!-- Features component -->
 <Features/>
-
-<!-- Statistics for businesses -->
-<Stats/>
 
 <!-- Libre Software component -->
 <Libre/>
