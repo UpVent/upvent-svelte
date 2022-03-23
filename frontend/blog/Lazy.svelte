5a887b6f6c472bcb10cfc73c37820c87d04ee111
VentGrey <omarpurataf@gmail.com>
2022-03-23 17:47:23 -0600

    Add lazy load component

---
+++ frontend/blog/Lazy.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let component;
+</script>
+
+{#await component.then ? component : component()}
+    Cargando...
+{:then Cmp}
+   <svelte:component this={Cmp.default} />
+{/await}
