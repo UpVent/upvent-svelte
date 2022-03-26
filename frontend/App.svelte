d22a654c17946a45bc844752ac38a90b92b11217
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-03-26 14:12:52 -0600

    Fix 35

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -69,7 +69,7 @@
 
 
 <!-- Navbar component -->
-<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-0 sticky-md-top navbar-upvent">
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-1 mb-0 sticky-md-top navbar-upvent">
     <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none navbar-brand">
         <img class="mx-auto img-fluid" width="148" height="51" src="/images/logo-grey.webp" alt="UpVent Logo">
     </a>
