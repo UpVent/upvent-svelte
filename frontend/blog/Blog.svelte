9227cbc57037b6e48118bbb20f2c748d0f32a51d
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-03-29 20:42:05 -0600

    Fix promises issue

--- frontend/blog/Blog.svelte
+++ frontend/blog/Blog.svelte
@@ -26,10 +26,10 @@
         {#await onMount}
             <p class="text-muted lead">Cargando publicaciones del blog...</p>
         {:then}
-            {#each posts as post, i}
+            {#each posts as post}
                 <div class="container">
                     <div class="card border-0 rounded-3 mt-5 mb-5 text-center">
-                        <img height="315" width="560" class="img-fluid mx-auto rounded-3" src={post._embedded['wp:featuredmedia'][i].media_details.sizes.full.source_url} alt="{post._embedded['wp:featuredmedia'][i].alt_text}">
+                        <img height="315" width="560" class="img-fluid mx-auto rounded-3" src={post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} alt="{post._embedded['wp:featuredmedia'][0].alt_text}">
                         <div class="card-body border-0">
                             <a class="fs-2 fw-bold text-primary text-decoration-none" href="/blog/post/{post.slug}">{post.title.rendered}</a>
                         </div>
