ac7dba6a43d936616b14f0a0ebf9453aafbb7452
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-03-29 20:59:28 -0600

    Revert lazyloading remove + fix promises error

--- frontend/blog/Blog.svelte
+++ frontend/blog/Blog.svelte
@@ -2,6 +2,9 @@
     // Svelte imports
     import { onMount } from 'svelte';
 
+    // Lazy Load post images
+    import Lazy from 'svelte-lazy';
+
     // Get API blog posts
     const api_url = "https://wpapi.upvent.codes/wp-json/wp/v2/posts?_embed";
 
@@ -29,7 +32,9 @@
             {#each posts as post}
                 <div class="container">
                     <div class="card border-0 rounded-3 mt-5 mb-5 text-center">
-                        <img height="315" width="560" class="img-fluid mx-auto rounded-3" src={post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} alt="{post._embedded['wp:featuredmedia'][0].alt_text}">
+                        <Lazy height={315}>
+                            <img height="315" width="560" class="img-fluid mx-auto rounded-3" src={post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} alt="{post._embedded['wp:featuredmedia'][0].alt_text}">
+                        </Lazy>
                         <div class="card-body border-0">
                             <a class="fs-2 fw-bold text-primary text-decoration-none" href="/blog/post/{post.slug}">{post.title.rendered}</a>
                         </div>
