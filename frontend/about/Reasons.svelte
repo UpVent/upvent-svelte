a4d3c7aeb6127aaa379878b6f4b5bcc644ca7541
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-03-31 22:42:07 -0600

    Resize icons

--- frontend/about/Reasons.svelte
+++ frontend/about/Reasons.svelte
@@ -22,23 +22,23 @@
 
         <div class="row pt-5">
             <div class="col-12 col-sm-6 col-md-3 text-center">
-                <h4 class="fw-bold">Soluciones Ligeras <LightningChargeFill class="text-primary"/></h4>
+                <h4 class="fw-bold">Soluciones Ligeras <LightningChargeFill width={24} height={24} class="text-primary"/></h4>
                 <p>Creamos nuestras soluciones personalizadas desde cero, todo el proceso de elaboración se realiza con código optimizado, rápido y flexible.
                 </p>
             </div>
 
             <div class="col-12 col-sm-6 col-md-3 pt-3 pt-sm-0 text-center">
-                <h4 class="fw-bold">Mejoras Constantes <CloudArrowUpFill class="text-primary"/></h4>
+                <h4 class="fw-bold">Mejoras Constantes <CloudArrowUpFill width={24} height={24} class="text-primary"/></h4>
                 <p>Todas nuestras soluciones personalizadas son actualizables a versiones con mejoras de seguridad o de rendimiento. ¡No mas obsolecencia programada!</p>
             </div>
 
             <div class="col-12 col-sm-6 col-md-3 pt-3 pt-md-0 text-center">
-                <h4 class="fw-bold">Desarrollo Universal <PhoneFill class="text-primary"/></h4>
+                <h4 class="fw-bold">Desarrollo Universal <PhoneFill width={24} height={24} class="text-primary"/></h4>
                 <p>Las soluciones ofrecidas por nosotros funcionan en todo tipo de dispositivos de uso común, desde laptops hasta tablets.</p>
             </div>
 
             <div class="col-12 col-sm-6 col-md-3 pt-3 pt-md-0 text-center">
-                <h4 class="fw-bold">Estabilidad Confiable <Stack class="text-primary"/></h4>
+                <h4 class="fw-bold">Estabilidad Confiable <Stack width={24} height={24} class="text-primary"/></h4>
                 <p>UpVent sigue un ciclo de desarrollo enfocado en la estabilidad y la escalabilidad del software, nuestras soluciones personalizadas no se romperán entre actualizaciones.</p>
             </div>
         </div>
