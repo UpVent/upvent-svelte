b82884d3cbf1d4af1d7919ab7969479a5cfcc271
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-04-06 19:23:07 -0500

    Fix #51

--- frontend/blog/Post.svelte
+++ frontend/blog/Post.svelte
@@ -21,9 +21,10 @@
     // Post properties
     let post_title;
     let post_description;
+    let post_image;
 
     // Post API URL
-    const api_url = "https://wpapi.upvent.codes/wp-json/wp/v2/posts?slug=" + route.params.slug;
+    const api_url = "https://wpapi.upvent.codes/wp-json/wp/v2/posts?slug=" + route.params.slug + "&_embed";
 
     // Fetch current post info
     onMount(async () => {
@@ -33,6 +34,8 @@
         post = post_json;
         post_title = post[0].title.rendered;
         post_description = post[0].description;
+        post_image = post[0]._embedded["wp:featuredmedia"][0].source_url;
+        console.log(post)
     });
 
 </script>
@@ -49,14 +52,14 @@
     <meta property="twitter:creator" content="@UpVentMX">
     <meta property="og:title" content="{post_title}">
     <meta property="og:description" content="{post_description}">
-    <meta property="og:image" content="https://upvent.codes/images/upvent-logo-new.webp">
+    <meta property="og:image" content="{post_image}">
 
     <!-- Twitter -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:url" content="@UpVentMX">
     <meta name="twitter:title" content="{post_title}">
     <meta name="twitter:description" content="{post_description}">
-    <meta name="twitter:image" content="https://upvent.codes/images/upvent-logo-new.webp">
+    <meta name="twitter:image" content="{post_image}">
 </svelte:head>
 
 <section class="container mt-5 mb-5">
@@ -64,6 +67,9 @@
         <p class="lead">Cargando post...</p>
     {:then data}
         {#each post as post}
+            <div class="container mx-auto text-center">
+                <img width="560" height="315" class="img-fluid rounded" src="{post_image}" alt="Título del blog"/>
+            </div>
             <p class="display-6">{post.title.rendered}</p>
             <hr>
 
