c547b8a940e1b971ea26c6ccab03e06c96e9a8b5
VentGrey <omarpurataf@gmail.com>
2022-09-08 17:57:38 -0500

    Add global variables for API

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,2 +1,3 @@
 /* Store for API Calls - This is similar to an angular service */
 export const api_url = 'https://wpapi.upvent.codes/wp-json/wp/v2/';
+export const uptime_api = 'https://api.uptimerobot.com/v2/getMonitors?api_key=ur1690087-9b94cc1a643f06e040d4b0a6';
