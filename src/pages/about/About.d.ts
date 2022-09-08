32f18f51013d5fe28ba1ed07372a678463c6be73
VentGrey <omarpurataf@gmail.com>
2022-09-08 17:56:58 -0500

    Add a type declaration

---
+++ src/pages/about/About.d.ts
@@ -0,0 +1,15 @@
+export type Project = {
+    descripcion_corta: string,
+    es_libre: string,
+    imagen_del_proyecto: {
+        guid: string
+    },
+    licencia: string,
+    link: string,
+    link_de_descarga: string,
+    nombre_del_proyecto: string,
+    sigue_activo: string,
+    title: {
+        rendered: string
+    }
+}
