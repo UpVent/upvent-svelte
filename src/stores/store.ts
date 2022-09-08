b3dc325e277487be9e366ab0a818fbc83295825c
VentGrey <omarpurataf@gmail.com>
2022-09-08 14:45:02 -0500

    Add api_url store usage

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,4 +1,2 @@
 /* Store for API Calls - This is similar to an angular service */
-import { readable } from "svelte/store";
-
-export const api_url = readable('https://wpapi.upvent.codes/wp-json/wp/v2');
+export const api_url = 'https://wpapi.upvent.codes/wp-json/wp/v2/';
