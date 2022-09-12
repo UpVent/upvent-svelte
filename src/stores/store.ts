9ee9b7c4fefa5236ba4a0c9c4e797c9239c0f42d
VentGrey <omarpurataf@gmail.com>
2022-09-11 22:50:18 -0500

    Add .env file handling for PROD / ENV API URLS

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,3 +1,3 @@
 /* Store for API Calls - This is similar to an angular service */
-export const api_url = 'https://wpapi.upvent.codes/wp-json/wp/v2/';
-export const uptime_api = 'https://api.uptimerobot.com/v2/getMonitors?api_key=ur1690087-9b94cc1a643f06e040d4b0a6';
+export const api_url: string = import.meta.env.VITE_API_URL;
+export const uptime_api = import.meta.env.VITE_UPTIME_API_URL;
