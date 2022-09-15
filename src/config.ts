b4792bb22ad44c2511df786b765f6222231926ce
VentGrey <omarpurataf@gmail.com>
2022-09-15 13:53:48 -0500

    Extract configuration values from .env file

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,6 @@
-/* Place your Firebase, Supabase or Pocketbase URL here */
+/* DBASE CONFIG */
 export const fapi_url: string = import.meta.env.VITE_DBASE_URL;
+
+/* DB USER AUTH CONFIG */
+export const api_user: string = import.meta.env.VITE_DBASE_USER;
+export const api_user_pass: string = import.meta.env.VITE_DBASE_USER_PASSWORD;
