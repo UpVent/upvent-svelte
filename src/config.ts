ed26d9166549ff12933e9007fadbb2b171003764
VentGrey <omarpurataf@gmail.com>
2022-09-18 17:35:14 -0500

    Migrate incident reporting to config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,6 @@ export const fapi_url: string = import.meta.env.VITE_DBASE_URL;
 /* DB USER AUTH CONFIG */
 export const api_user: string = import.meta.env.VITE_DBASE_USER;
 export const api_user_pass: string = import.meta.env.VITE_DBASE_USER_PASSWORD;
+
+/** INCIDENT MONITORING CONFIG */
+export const uptime_api = import.meta.env.VITE_UPTIME_API_URL;
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,3 +1,2 @@
 /* Store for API Calls - This is similar to an angular service */
 export const api_url: string = import.meta.env.VITE_API_URL;
-export const uptime_api = import.meta.env.VITE_UPTIME_API_URL;
