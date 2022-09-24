dff99f394531d832a22994dc8ad908bf862e0393
VentGrey <omarpurataf@gmail.com>
2022-09-24 17:39:34 -0500

    Migrate blog to fetch from pocketbase instead

--- src/pages/blog/Blog.svelte
+++ src/pages/blog/Blog.svelte
@@ -2,23 +2,28 @@
 <script lang="ts">
     // Svelte imports
     import { onMount } from 'svelte';
-    import { api_url } from '../../stores/store';
-    import type { Posts } from './Blog';
+    import { fapi_url, api_user, api_user_pass } from '../../config';
+    import { truncate_str } from '../../stores/store';
 
     // Lazy Load post images
     import Lazy from 'svelte-lazy';
 
-    // Get API blog posts
-    const url: string = api_url + "posts?_embed";
+    // Database imports
+    import PocketBase from 'pocketbase';
+    import type { Record } from 'pocketbase';
 
-    // Blog Posts array
-    let posts: Posts[] = [];
+    // Database usage
+    const client: PocketBase = new PocketBase(fapi_url);
+
+    // Grid logos for blog showcasing
+    let records: Record[] = [];
 
     // Get blog posts on component mount
     onMount(async () => {
-        // Posts request
-        const posts_res: Response = await fetch(url);
-        posts = await posts_res.json();
+        const user_auth_data = await client.users.authViaEmail(api_user, api_user_pass);
+        records = await client.records.getFullList('blog_post', 200, { sort: '-created' });
+        records.forEach(e => delete e.contenido) 
+        client.authStore.clear();
     });
 </script>
 
@@ -30,17 +35,17 @@
         {#await onMount}
             <p class="text-muted lead">Cargando publicaciones del blog...</p>
         {:then}
-            {#each posts as post}
+            {#each records as record}
                 <div class="container">
                     <div class="card border-0 rounded-3 mt-5 mb-5 text-center">
                         <Lazy height={315}>
-                            <img height="315" width="560" class="img-fluid mx-auto rounded-3" src={post._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} alt="{post._embedded['wp:featuredmedia'][0].alt_text}">
+                            <img height="315" width="560" class="img-fluid mx-auto rounded-3" src={client.records.getFileUrl(record, record.imagen_destacada)} alt="Imágen de blog">
                         </Lazy>
                         <div class="card-body border-0">
-                            <a class="fs-2 text-primary text-decoration-none" href="/blog/post/{post.slug}">{post.title.rendered}</a>
+                            <a class="fs-2 text-primary text-decoration-none" href="/blog/post/{record.slug}">{record.titulo}</a>
                         </div>
                         <div class="card-footer border-top">
-                            <p class="text-muted small">{@html post.excerpt.rendered}</p>
+                            <p class="text-muted small">{truncate_str(record.extracto, 150)}</p>
                         </div>
                     </div>
                 </div>
