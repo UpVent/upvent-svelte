8e721ef510dd1b7dcd35c8d39165bd538e624602
VentGrey <omarpurataf@gmail.com>
2022-09-24 17:38:58 -0500

    Add a shared function for truncating strings

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,2 +1,7 @@
 /* Store for API Calls - This is similar to an angular service */
 export const api_url: string = import.meta.env.VITE_API_URL;
+
+/** Stored function for string truncation */
+export function truncate_str(str: string, limit: number): string {
+     return str.length > limit ? str.slice(0, limit) + '...' : str;
+}
