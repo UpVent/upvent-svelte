66d12898ece9c3c55b3741694380e474f87848b3
VentGrey <omarpurataf@gmail.com>
2022-09-24 22:04:13 -0500

    Add a lazy loading for components + codesplitting

---
+++ src/lib/LL.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let component: any;
+</script>
+
+{#await component.then ? component : component()}
+    Cargando...
+{:then Cmp}
+   <svelte:component this={Cmp.default} />
+{/await}
