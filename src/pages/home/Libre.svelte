1e43a4d9873a5ce33e184b081e5a3d419cdc0a67
VentGrey <omarpurataf@gmail.com>
2022-10-17 18:00:44 -0500

    Update dependencies

--- src/pages/home/Libre.svelte
+++ src/pages/home/Libre.svelte
@@ -5,7 +5,6 @@
     <p class="h2 fw-bold">Todo lo que hacemos es de</p> <span class="h2 fw-bold text-glow text-primary">código libre</span>
     <p class="text-center text-wrap lead">
             Nuestro modelo de desarrollo consiste totalmente de componentes de código
-            libre y se basa en múltiples principios éticos y prácticos para su conveniencia como empresa.<br />
-            Además, colaboramos de manera activa con el ecosistema de software libre en México para mejorar dichas tecnologías para todos.
+            libre y se basa en múltiples principios éticos y prácticos para su conveniencia como empresa.
     </p>
 </section>
