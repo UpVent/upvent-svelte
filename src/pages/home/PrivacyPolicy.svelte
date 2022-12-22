30fe6b342137877bfd47777af5635aac1a1206bb
VentGrey <omarpurataf@gmail.com>
2022-12-21 23:12:26 -0600

    Migrate PrivacyPolicy to pbwrap

--- src/pages/home/PrivacyPolicy.svelte
+++ src/pages/home/PrivacyPolicy.svelte
@@ -1,31 +1,18 @@
 <script lang="ts">
-    // Svelte imports
-    import { onMount, onDestroy } from 'svelte';
-    import { fapi_url, api_user, api_user_pass } from '../../config';
-    import { api_result } from '../../stores/store';
-
-    /** Database Imports */
+    import { fapi_url } from '../../config';
+    import { getTables } from '../../helpers/pbwrap';
     import PocketBase from 'pocketbase';
-
-    // Database usage
+    import type { Record } from 'pocketbase';
     const pb: PocketBase = new PocketBase(fapi_url);
-
-    onMount(async () => {
-        await pb.collection('users').authWithPassword(api_user, api_user_pass);
-        $api_result = await pb.collection('politica_privacidad').getFullList(1);
-        pb.authStore.clear();
-    });
-
-    onDestroy(() => { $api_result.length = 0; });
-
+    const records: Promise<Record[]> = getTables(pb, 'politica_privacidad');
     function format_date(date: string): string { return new Date(date).toLocaleDateString('es-MX'); }
 </script>
 
 <section class="container">
-    {#each $api_result as record}
+    {#await records} <p>Cargando política de privacidad...</p>{:then data}     
+    {#each data as record}
         <h1>{record.titulo}</h1>
         <span class="badge rounded-pill text-bg-primary">Última revisión: { format_date(record.ultima_revision) }</span>
         <hr>
         <section class="container"> {@html record.contenido } </section>
-    {/each}
-</section>
+    {/each}{/await}</section>
