285fc62ee3f5c2aa9fba5187cc09701c50a395d7
VentGrey <omarpurataf@gmail.com>
2023-01-12 00:21:19 -0600

    Turn 'portfolio-showcase' into a dumb component

---
+++ src/routes/about/portfolio/+page.server.ts
@@ -0,0 +1,29 @@
+import { getTables } from "$lib/common/pbwrap";
+
+import { env } from '$env/dynamic/private';
+import PocketBase from "pocketbase";
+ import type { Record } from "pocketbase";
+
+export const load = async () => {
+    const pb: PocketBase = new PocketBase(env.VITE_DBASE_URL);
+
+    await pb.collection('users').authWithPassword(env.VITE_DBASE_USER, env.VITE_DBASE_USER_PASSWORD);
+    const records: Record[] = await getTables(pb, 'proyecto_portafolio');
+
+    const imgPromises = records.map(async (r) => {
+        return pb.getFileUrl(r, r.imagen_destacada);
+    });
+
+    const imgUrls = await Promise.all(imgPromises);
+
+     // Agregar las url de las imágenes al objeto
+    const projects = records.map((r, i) => {
+        const project = r.export();
+        project.img = imgUrls[i];
+        return project;
+    });
+
+    pb.authStore.clear();
+
+    return { projects };
+}
--- src/routes/about/portfolio/+page.svelte
+++ src/routes/about/portfolio/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
  /* Svelte / Internal imports */
- import { fapi_url } from "$lib/common/settings";
- import { getTables } from "$lib/common/pbwrap";
  import SEO from "$lib/SEO.svelte";
- /* External imports */
- import PocketBase from "pocketbase";
- import type { Record } from "pocketbase";
+ import type { PageData } from "./$types";
 
- /* Logic */
- const pb: PocketBase = new PocketBase(fapi_url);
- const records: Promise<Record[]> = getTables(pb, 'proyecto_portafolio');
+/* Logic */
+ export let data: PageData;
+ const records = data.projects;
 </script>
 
 <SEO/>
@@ -22,38 +18,18 @@
 
 <section class="container py-2">
     <div class="row row-cols-2">
-        {#await records}
-            <div class="col placeholder-glow">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>                <span class="placeholder col-7"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-6"></span>
-                <span class="placeholder col-8"></span>
-            </div>
-
-            <div class="col placeholder-glow">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect></svg>                <span class="placeholder col-7"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-4"></span>
-                <span class="placeholder col-6"></span>
-                <span class="placeholder col-8"></span>
-            </div>
-        {:then records}
-            {#each records as record}
-                <div class="col px-2 gy-3">
-                    <div class="container">
-                        <img height="100" class="img-fluid m-1 p-1 shadow-sm rounded-3" src="{ pb.getFileUrl(record, record.imagen_destacada) }" alt="proyecto de UpVent">
-                        <p class="fw-bold">{record.nombre}</p>
-                        <p class="small">
-                            {@html record.descripcion}
-                        </p>
-                        <div class="d-grid">
-                            <a class="btn btn-primary" href="/about/portfolio/{record.id}">Ver proyecto</a>
-                        </div>
-                    <hr />
+        {#each records as record}
+            <div class="col px-2 gy-3">
+                <div class="container">
+                    <img height="100" class="img-fluid m-1 p-1 shadow-sm rounded-3" src="{record.img}" alt="proyecto de UpVent">
+                    <p class="fw-bold">{record.nombre}</p>
+                    <p class="small">{@html record.descripcion}</p>
+                    <div class="d-grid">
+                        <a class="btn btn-primary" href="/about/portfolio/{record.id}">Ver proyecto</a>
                     </div>
+                <hr />
                 </div>
-            {/each}
-        {/await}
+            </div>
+        {/each}
     </div>
 </section>
